58f9c7cf5d99dcc7b1a00597461640f845733dff
jatorres <jatorres@alumnos.inf.utfsm.cl>
2024-06-20 21:07:08 -0400

    Cambio de puerto para S.O general

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -5,19 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-// Declaración de las consultas GraphQL como constantes
-const LOGIN_QUERY = gql`
-  query Login($datoslog1: login) {
-    login(input: $datoslog1) {
-      rut
-      nrol
-      mensaje
-    }
-  }
-`;
-
-
-
 // Creación del cliente Apollo
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
